fd4342d4fe666d751bd5435766ff40dbd188552c
Seneca <seneca.rubianes.bvt2023@gmail.com>
2024-06-19 19:06:14 -0700

    first commit

---
+++ practice-one.js
@@ -0,0 +1,27 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+
+let exampleObject = {
+    id: 1,
+    name: "your name"
+}
+
+
+console.log(exampleObject);
+console.log("My name is: ",exampleObject.name);
+
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i =0; i <10; i++){
+        console.log(i);
+    }
+}
+
+
+doSomething();
